829feeb9082267294b6ccf7e6253c9c7b118c083
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-06-18 00:56:29 +0530

    UI design complete

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div  className='overlay'>
+                <h1>Quiz App</h1>
+                <h3>Test your knowledge</h3>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/quiz/Quiz.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './quiz.css'
+
+const Quiz = () => {
+    return (
+
+ 
+<div className='container'>
+<div className="question">
+
+</div>
+<div className="option-container">
+
+</div>
+</div>
+    )
+}
+
+export default Quiz
