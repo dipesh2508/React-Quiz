29f5f79fc058269ed6597c83f4ec96071196d54e
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-06-18 21:36:16 +0530

    first stage logic applied

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './quiz.css'
+import { QuizData } from '../../data/QuizData'
+
+
 
 const Quiz = () => {
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [score, setScore] = useState(0)
+    const [selected, setSelected] = useState(0)
+
+    const changeQuestion = () =>{
+        updateScore();
+        if(currentQuestion < QuizData.length-1){
+            setCurrentQuestion(currentQuestion+1)
+        }
+
+    }
+
+    const updateScore = () =>{
+        if(selected === QuizData[currentQuestion].answer){
+            setScore(score+1)
+        }
+    }
+
     return (
 
- 
-<div className='container'>
-<div className="question">
+        <div className='container'>
+            <div className="question">
+                <span id='question-number'>
+                {currentQuestion+1}.
+                </span>
+                <span id='question-txt'>
+                    {QuizData[currentQuestion].question}
+                </span>
+            </div>
+            <div className="option-container">
+            {QuizData[currentQuestion].options.map((options, i)=>{
+                return(
+                    <button className={`option-btn ${selected == i+1?'checked':null}`} key={i} onClick={()=> setSelected(i+1)}>
+                        {options}
+                    </button>
+                )
+            })}
 
-</div>
-<div className="option-container">
-
-</div>
-</div>
+            <input type="button" value="Next" id='next-button' onClick={changeQuestion}/>
+            </div>
+        </div>
     )
 }
 
---
+++ src/data/QuizData.jsx
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question: 'Which Pokemon has the ability "Levitate," making it immune to Ground-type moves?',
+        options:[ 'Gengar', 'Flygon', 'Crobat', 'Rotom'],
+        answer: 1
+    },
+    {
+        question: "Which Pokemon type is not super effective against any other type?",
+        options:['Fairy', 'Normal', 'Psychic', 'Electric'],
+        answer: 2
+    },
+    {
+        question: "Which Pokemon is a dual-type Dragon/Flying Pokemon and is said to be the guardian of the skies?",
+        options: ['Rayquaza', 'Salamence', 'Dragonite', 'Altaria'],
+        answer: 1
+    },
+    {
+        question: "Which Pokemon has the highest base HP stat?",
+        options: ['Wobbuffet', 'Snorlax', 'Chansey', 'Blissey'],
+        answer:4
+    },
+    {
+        question: 'In the Pokemon games, which type is super effective against both Grass and Electric types?',
+        options: ['Fire', 'Bug', 'Ground', 'Ice'],
+        answer: 3
+    }
+];
