fcd9ea1ce7bb3299b036cada1fc18e013dfd9248
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-06-20 00:07:41 +0530

    progress bar added

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import './result.css'
 
 const Result = (props) => {
   return (
-    <div>
-    Score: {props.score}<br />
-    Total Score : {props.totalScore}
-    </div> 
+
+    <div class="progress-section" data-aos="fade-left" data-aos-once="true">
+      <div class="task-progress">
+        <p>Your Score
+          <span>{props.score}</span>
+        </p>
+        <progress className='progress' max={props.totalScore} value={props.score}/>
+      </div>
+    </div>
+
   )
 }
 
