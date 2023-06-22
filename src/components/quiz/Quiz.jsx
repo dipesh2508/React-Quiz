1e7a4904a1ce149dde2a9fecf8e7649f3fd118d3
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-06-22 14:40:42 +0530

    logic implemented

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -10,58 +10,93 @@ const Quiz = () => {
     const [score, setScore] = useState(0);
     const [selected, setSelected] = useState(0);
     const [showResult, setShowResult] = useState(0);
+    const [currentGenre, setCurrentGenre] = useState(0);
+    const [genreContainer, setGenreContainer] = useState(false);
 
     const changeQuestion = () => {
         updateScore();
-        if (currentQuestion < QuizData.length - 1) {
+        if (currentQuestion < QuizData[0].data[currentGenre].length - 1) {
             setCurrentQuestion(currentQuestion + 1);
             setSelected(0);
         }
-        else{
+        else {
             setShowResult(true);
         }
 
     }
 
+    const genreSelection = () => {
+        setCurrentGenre(selected - 1);
+        setGenreContainer(true);
+        setSelected(0);
+
+    }
+
     const updateScore = () => {
-        if (selected === QuizData[currentQuestion].answer) {
+        if (selected === QuizData[0].data[currentGenre][currentQuestion].answer) {
             setScore(score + 1)
         }
     }
 
     return (
-
         <div className='container'>
-        {showResult ? ( 
-            <Result score={score} totalScore={QuizData.length}/>
-            ):(
-            <>
-            <div className="question">
-                <span id='question-number'>
-                    {currentQuestion + 1}.
-                </span>
-                <span id='question-txt'>
-                    {QuizData[currentQuestion].question}
-                </span>
-            </div>
-            <div className="option-container">
-                {QuizData[currentQuestion].options.map((option, i) => {
-                    return (
-                        <button className={`option-btn ${selected === i + 1 ? 'checked' : null}`}
-                            key={i}
-                            onClick={() => setSelected(i + 1)}
-                        >
-                            {option}
-                        </button>
-                    )
-                })}
-                </div>
-                <input type="button" value="Next" id='next-button' onClick={changeQuestion} />
+            {showResult ? (
+                <Result score={score} totalScore={QuizData[0].data[currentGenre].length} />
+            ) : (
+                <>
+                    {genreContainer ? (
+                        <>
+                            <div className="question">
+                                <span id='question-number'>
+                                    {currentQuestion + 1}.
+                                </span>
+                                <span id='question-txt'>
+                                    {QuizData[0].data[currentGenre][currentQuestion].question}
+                                </span>
+                            </div>
+                            <div className="option-container">
+                                {QuizData[0].data[currentGenre][currentQuestion].options.map((option, i) => {
+                                    return (
+                                        <button className={`option-btn ${selected === i + 1 ? 'checked' : null}`}
+                                            key={i}
+                                            onClick={() => setSelected(i + 1)}
+                                        >
+                                            {option}
+                                        </button>
+                                    )
+                                })}
+                            </div>
+                            <input type="button" value="Next" id='next-button' onClick={changeQuestion} />
+                        </>
+                    ) : (
+                        <>
+                            <div className="question">
+                                <span id='question-txt'>
+                                    Choose your genre of Quiz
+                                </span>
+                            </div>
+                            <div className="option-container">
+                                {QuizData[0].options.map((option, i) => {
+                                    return (
+                                        <button className={`option-btn ${selected === i + 1 ? 'checked' : null}`}
+                                            key={i}
+                                            onClick={() => {
+                                                setSelected(i + 1);
+                                            }}
+                                        >
+                                            {option}
+                                        </button>
+                                    )
+                                })}
+                            </div>
+                            <input type="button" value="Select" id='next-button' onClick={genreSelection} />
+                            
+                        </>
+                    )}
+                    
                 </>
-            )} 
+            )}
         </div>
-
     )
 }
-
 export default Quiz
--- src/data/QuizData.jsx
+++ src/data/QuizData.jsx
@@ -1,72 +1,85 @@
-export const QuizData = [
-    {
-        question: 'Which Pokemon has the ability "Levitate," making it immune to Ground-type moves?',
-        options:[ 'Gengar', 'Flygon', 'Crobat', 'Rotom'],
-        answer: 1
-    },
-    {
-        question: "Which Pokemon type is not super effective against any other type?",
-        options:['Fairy', 'Normal', 'Psychic', 'Electric'],
-        answer: 2
-    },
-    {
-        question: "Which Pokemon is a dual-type Dragon/Flying Pokemon and is said to be the guardian of the skies?",
-        options: ['Rayquaza', 'Salamence', 'Dragonite', 'Altaria'],
-        answer: 1
-    },
-    {
-        question: "Which Pokemon has the highest base HP stat?",
-        options: ['Wobbuffet', 'Snorlax', 'Chansey', 'Blissey'],
-        answer:4
-    },
-    {
-        question: 'In the Pokemon games, which type is super effective against both Grass and Electric types?',
-        options: ['Fire', 'Bug', 'Ground', 'Ice'],
-        answer: 3
-    },
-    {
-        question: 'What is the name of the Pokemon that can learn every move in the game?',
-        options: ['Mewtwo', 'Ditto', 'Smeargle', 'Arceus'],
-        answer: 3
-    },
-    {
-        question: 'Which Pokemon has the highest base Special Attack stat?',
-        options: ['Mewtwo', 'Alakazam', 'Gengar', 'Espeon'],
-        answer: 1
-    },
-    {
-        question: 'Which Pokemon type is immune to poison-type moves?',
-        options: ['fire', 'steel', 'dark', 'water'],
-        answer: 2
-    },
-    {
-        question: 'Garchomp is known for its exceptional speed and power. What is its base Speed stat?',
-        options: ['90', '100', '110', '120'],
-        answer: 3
-    },
-    {
-        question: 'What is the signature move of Typhlosion?',
-        options: ['Blast burn', 'Overheat', 'Fire blast', 'Flamethrower'],
-        answer: 1
-    },
-    {
-        question: ' In the Pokémon anime series, which Pokémon does Gary Oak evolve his Eevee into?',
-        options: ['Espeon', 'Leafeon', 'Vapereon', 'Umbreon'],
-        answer: 4
-    },
-    {
-        question: 'What is the signature Pokémon of Steven, the Champion of the Hoenn region?',
-        options: ['Aggron', 'Metagross', 'Skarmory', 'Claydol'],
-        answer:2
-    },
-    {
-        question: 'What does STAB mean?',
-        options: ['Same Type Attack Boost', 'Special Technique Amplification Bonus', 'Same Type Attack Bonus', 'Super Effective Attack Boost'],
-        answer: 3
-    },
-    {
-        question: "What was ash's first shiny pokemon",
-        options: ['Noctowl', 'Litten', 'Poipole', 'Butterfree'],
-        answer: 1
-    }
-];
+export const QuizData = [{
+    options: ['Pokemon', 'trial'],
+    data: [
+        [{
+            question: 'Which Pokemon has the ability "Levitate," making it immune to Ground-type moves?',
+            options: ['Gengar', 'Flygon', 'Crobat', 'Rotom'],
+            answer: 1
+        },
+        {
+            question: "Which Pokemon type is not super effective against any other type?",
+            options: ['Fairy', 'Normal', 'Psychic', 'Electric'],
+            answer: 2
+        },
+        {
+            question: "Which Pokemon is a dual-type Dragon/Flying Pokemon and is said to be the guardian of the skies?",
+            options: ['Rayquaza', 'Salamence', 'Dragonite', 'Altaria'],
+            answer: 1
+        },
+        {
+            question: "Which Pokemon has the highest base HP stat?",
+            options: ['Wobbuffet', 'Snorlax', 'Chansey', 'Blissey'],
+            answer: 4
+        },
+        {
+            question: 'In the Pokemon games, which type is super effective against both Grass and Electric types?',
+            options: ['Fire', 'Bug', 'Ground', 'Ice'],
+            answer: 3
+        },
+        {
+            question: 'What is the name of the Pokemon that can learn every move in the game?',
+            options: ['Mewtwo', 'Ditto', 'Smeargle', 'Arceus'],
+            answer: 3
+        },
+        {
+            question: 'Which Pokemon has the highest base Special Attack stat?',
+            options: ['Mewtwo', 'Alakazam', 'Gengar', 'Espeon'],
+            answer: 1
+        },
+        {
+            question: 'Which Pokemon type is immune to poison-type moves?',
+            options: ['fire', 'steel', 'dark', 'water'],
+            answer: 2
+        },
+        {
+            question: 'Garchomp is known for its exceptional speed and power. What is its base Speed stat?',
+            options: ['90', '100', '110', '120'],
+            answer: 3
+        },
+        {
+            question: 'What is the signature move of Typhlosion?',
+            options: ['Blast burn', 'Overheat', 'Fire blast', 'Flamethrower'],
+            answer: 1
+        },
+        {
+            question: ' In the Pokémon anime series, which Pokémon does Gary Oak evolve his Eevee into?',
+            options: ['Espeon', 'Leafeon', 'Vapereon', 'Umbreon'],
+            answer: 4
+        },
+        {
+            question: 'What is the signature Pokémon of Steven, the Champion of the Hoenn region?',
+            options: ['Aggron', 'Metagross', 'Skarmory', 'Claydol'],
+            answer: 2
+        },
+        {
+            question: 'What does STAB mean?',
+            options: ['Same Type Attack Boost', 'Special Technique Amplification Bonus', 'Same Type Attack Bonus', 'Super Effective Attack Boost'],
+            answer: 3
+        },
+        {
+            question: "What was ash's first shiny pokemon",
+            options: ['Noctowl', 'Litten', 'Poipole', 'Butterfree'],
+            answer: 1
+        }],
+        [{
+            question: 'this is a sample questions',
+            options: ['option1', 'option2', 'option3', 'option4'],
+            answer: 1
+        },
+        {
+            question: 'this is another sample questions',
+            options: ['option1', 'option2', 'option3', 'option4'],
+            answer: 2
+        }]
+    ]
+}];
